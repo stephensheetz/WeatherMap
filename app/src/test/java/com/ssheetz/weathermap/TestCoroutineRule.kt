c126953007923a9cae68cca40f5f7f9d9f023d4f
ssheetz <stephen.sheetz@gmail.com>
2022-08-29 17:16:41 -0400

    Fix MainActivityViewModel unit test

---
+++ app/src/test/java/com/ssheetz/weathermap/TestCoroutineRule.kt
@@ -0,0 +1,32 @@
+package com.ssheetz.weathermap
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.*
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+/*
+  Thanks to
+  https://cdmunoz.medium.com/adding-unit-tests-to-an-android-project-that-uses-coroutines-and-livedata-323a4ce53c62
+ */
+@ExperimentalCoroutinesApi
+class TestCoroutineRule : TestRule {
+
+    private val testCoroutineDispatcher = TestCoroutineDispatcher()
+    private val testCoroutineScope = TestCoroutineScope(testCoroutineDispatcher)
+
+    override fun apply(base: Statement, description: Description): Statement = object : Statement() {
+        @Throws(Throwable::class)
+        override fun evaluate() {
+            Dispatchers.setMain(testCoroutineDispatcher)
+            base.evaluate()
+            Dispatchers.resetMain()
+            testCoroutineScope.cleanupTestCoroutines()
+        }
+    }
+
+    fun runBlockingTest(block: suspend TestCoroutineScope.() -> Unit) =
+        testCoroutineScope.runBlockingTest { block() }
+}
--- app/src/test/java/com/ssheetz/weathermap/view/MainActivityViewModelTest.kt
+++ app/src/test/java/com/ssheetz/weathermap/view/MainActivityViewModelTest.kt
@@ -2,23 +2,29 @@ package com.ssheetz.weathermap.view
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.Observer
+import com.ssheetz.weathermap.TestCoroutineRule
 import com.ssheetz.weathermap.model.ForecastData
 import com.ssheetz.weathermap.model.ForecastElement
 import com.ssheetz.weathermap.model.ForecastPlace
 import com.ssheetz.weathermap.repository.Repository
 import com.ssheetz.weathermap.viewmodel.MainActivityViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flow
 import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentCaptor
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 import org.mockito.Mockito
 import org.mockito.Mockito.anyDouble
 import org.mockito.MockitoAnnotations
+import org.mockito.junit.MockitoJUnitRunner
 import org.mockito.kotlin.mock
 
 
+@ExperimentalCoroutinesApi
+@RunWith(MockitoJUnitRunner::class)
 class MainActivityViewModelTest {
 
     lateinit var viewModel: MainActivityViewModel
@@ -26,8 +32,13 @@ class MainActivityViewModelTest {
     lateinit var repository: Repository
     lateinit var sampleData: ForecastData
 
+    // Needed to test with LiveData
     @get:Rule
-    val rule = InstantTaskExecutorRule()
+    val testInstantTaskExecutorRule: TestRule = InstantTaskExecutorRule()
+
+    // Needed to test with coroutines
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRule()
 
     @Before
     fun setUp() {
@@ -37,7 +48,7 @@ class MainActivityViewModelTest {
         searchObserver = mock()
 
         sampleData = ForecastData(
-            ForecastPlace(123, "some city", -88.1, 33.5), listOf(
+            ForecastPlace(123L, "some city", -88.1, 33.5), listOf(
             ForecastElement(0, 123, 1002, "mainname", "some weather conditions", "someicon.png", 44.4f, 33.3f, 950f,77f, 23.1f, 223.0f)
         ))
 
@@ -47,18 +58,20 @@ class MainActivityViewModelTest {
 
     @Test
     fun forecastUpdatesLiveDataWithForecastFromRepository() {
-        val arg = ArgumentCaptor.forClass(ForecastData::class.java)
-        //verify(searchObserver).onChanged(arg.capture())
-        viewModel.getResultsObserver().observeForever(searchObserver)
+        testCoroutineRule.runBlockingTest {
 
-        viewModel.tapNewLocation(-45.1, 44.1, 7.0)
+            viewModel.getResultsObserver().observeForever(searchObserver)
+            Mockito.`when`(repository.getSavedLocations()).thenReturn(
+                listOf( ForecastPlace(114L, "", -23.2, 44.5) )
+            )
 
-        val forecasts = viewModel.getResultsObserver().value?.forecasts
-        val place = viewModel.getResultsObserver().value?.place
-        //val forecasts = arg.allValues[0].forecasts
-        //val place = arg.allValues[0].place
-        assertEquals(1, forecasts?.size)
-        assertEquals(123, place?.id)
+            viewModel.tapNewLocation(-45.1, 44.1, 7.0)
+
+            val forecasts = viewModel.getResultsObserver().value?.forecasts
+            val place = viewModel.getResultsObserver().value?.place
+            assertEquals(1, forecasts?.size)
+            assertEquals(123L, place?.id)
+        }
     }
 }
 
